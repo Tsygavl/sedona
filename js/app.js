7adfb719b22c09d373688f96b9f81c2e5af527d3
Nikolai <rus.nick@mail.ru>
2016-10-08 10:48:45 +0300

    Описана логика модальных окон для мобильной версии

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ btnOpenNavigation.addEventListener("click", function() {
 
 
 btnCloseNavigation.addEventListener("click", function() {
-	
+
 	if (!mainNvaigationItems.classList.contains("visually-hidden")) {
 
 		mainNvaigationItems.classList.add("visually-hidden");
@@ -31,3 +31,46 @@ btnCloseNavigation.addEventListener("click", function() {
 		btnCloseNavigation.classList.add("visually-hidden");
 	}
 });
+
+/*
+ * Всплывающие дилоговые окна
+ */
+
+var recallForm = document.querySelector(".recall-form");
+
+if (recallForm) {
+
+    var submitButton = recallForm.querySelector("input[type=submit]");
+
+    var failureDialog = document.querySelector(".modal-dialog--failure");
+    var successDialog = document.querySelector(".modal-dialog--success");
+
+    submitButton.addEventListener("click", function(event) {
+      event.preventDefault();
+
+      if (recallForm.checkValidity()) {
+          // Форма валидна
+          successDialog.classList.add("modal-dialog--show");
+
+      } else {
+          // Форма заполнена с ошибками
+          failureDialog.classList.add("modal-dialog--show");
+      }
+    });
+
+    var failureDialogOkBtn = failureDialog.querySelector(".modal-dialog__btn-ok");
+
+    failureDialogOkBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        failureDialog.classList.remove("modal-dialog--show");
+    })
+
+    var successDialogCancelBtn = successDialog.querySelector(".modal-dialog__btn-cancel");
+
+    successDialogCancelBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        successDialog.classList.remove("modal-dialog--show");
+    })
+}
